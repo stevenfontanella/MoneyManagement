c5c23acc792f6b7cd389fa551696347862961a42
mciganik <47087630+mciganik@users.noreply.github.com>
2019-11-07 11:47:23 -0500

    Connected Front to back, added datatable, put data in graph

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -2,38 +2,63 @@ import logo from './capital-one-logo2.png';
 import './App.css';
 //import tableData from './data.json';
 import { VictoryStack, VictoryArea } from 'victory';
-import { VictoryBar, VictoryChart, VictoryAxis } from 'victory';
+//import { VictoryBar, VictoryChart, VictoryAxis } from 'victory';
 import { JsonToTable } from "react-json-to-table";
 import React, { Component } from 'react';
-import tableData from './data.json';
+//import tableData from './data.json';
+
+import { MDBDataTable } from 'mdbreact';
 
 const API = 'http://localhost:5000/transactions';
 const DEFAULT_QUERY = '/';
+const budget = 35000
+var total = 0;
+var display = true
 
 function sumCat(cat,arr){
 	var sum = 0;
 	var rollingSum = [];
+	var temp = {x: 0, y:0}
 	for(var i in arr){
 		if(cat == "all"){
 			sum = arr[i].amount + sum;
-			var temp = { x : i, y: sum}
-			rollingSum.push(temp)
+			temp = { x : i, y: sum}
+			total = sum;
+			rollingSum.push(temp);
 		}else{
 			if(cat == arr[i].category){
 				sum = arr[i].amount + sum;
-				var temp = { x : i, y: sum}
-				rollingSum.push(temp)	
+				temp = { x : i, y: sum};
+				rollingSum.push(temp);	
 			}else{
-			var temp = { x : i, y: sum}
-			rollingSum.push(temp)			
+				temp = { x : i, y: sum};
+				rollingSum.push(temp);			
 			}
 		}		
 	}	
 	return rollingSum;
 }
+
+function checkBudget(){
+	if (total >= budget){
+		return(
+        <div className="App-warning-bar">
+          <p>WARNING: OVER MONTHLY LIMIT</p>
+        </div>)
+    }else if ((budget-total) <= (budget*0.1)){
+		return(
+			<div className="App-caution-bar">
+				<p>CAUTION: CLOSE TO MONTHLY LIMIT</p>
+			</div>)	
+	}
+	
+	
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
+	this.displays = [true,true,true,true,true,true,true,true,true]
     this.state = {
       hits: [],
 	  isLoading: false
@@ -47,7 +72,24 @@ class App extends Component {
       .then(data => this.setState({ hits: data , isLoading: false }));
   }
    
-  
+  writeArea(cat,cond){
+	if(cond){
+	var rollingSum = sumCat(cat,this.state.hits);
+	
+	return (<VictoryArea
+              data={rollingSum}
+			  labels={({ data, index }) => index == data.length - 1 ? cat : ""}
+            />)		
+			}	
+	}
+	invert(){
+		var checkBox = document.getElementById("OtherBox");
+		if (checkBox.checked == true){
+			this.displays[8] = true;
+		} else {
+			this.displays[8] = false;
+		}
+	}
   render() {
     var { hits, isLoading } = this.state;
 	var cats = ["Grocery","Merchandise","Other","Entertainment","Dining","Travel","Gas/Automotive","Insurance","Clothing"]
@@ -59,19 +101,54 @@ class App extends Component {
 	for (var i in hits) {
         hits[i].user = "temp";
      }
-	 
 	var rollingSumAll = sumCat("all",hits);
-	var rollingSumGroc = sumCat("Grocery",hits);
-	var rollingSumMerc = sumCat("Merchandise",hits);
-	var rollingSumOther = sumCat("Other",hits);
-	var rollingSumEnt = sumCat("Entertainment",hits);
-	var rollingSumDine = sumCat("Dining",hits);
-	var rollingSumTrav = sumCat("Travel",hits);
-	var rollingSumAuto = sumCat("Gas/Automotive",hits);
-	var rollingSumInsu = sumCat("Insurance",hits);
-	var rollingSumCloth = sumCat("Clothing",hits);
-	 
-	 
+	var data = {
+		columns: [
+      {
+        label: 'Amount',
+        field: 'amount',
+        sort: 'asc',
+        width: 200
+      },
+      {
+        label: 'Category',
+        field: 'category',
+        sort: 'asc',
+        width: 200
+      },
+      {
+        label: 'Date',
+        field: 'date',
+        sort: 'asc',
+        width: 250
+      },
+      {
+        label: 'ID',
+        field: 'id',
+        sort: 'asc',
+        width: 75
+      },
+      {
+        label: 'User',
+        field: 'user',
+        sort: 'asc',
+        width: 50
+      },
+      {
+        label: 'Location',
+        field: 'vendorLocation',
+        sort: 'asc',
+        width: 300
+      },
+	  {
+        label: 'Vendor',
+        field: 'vendorName',
+        sort: 'asc',
+        width: 300
+      }
+    ],
+  rows: hits
+  };
 	return (
       <div className="App">
       <header className="App-header">
@@ -79,46 +156,28 @@ class App extends Component {
         <h1>
           Money Management
         </h1>
-        <div className="App-status-bar">
-          <p>CAUTION: Near Monthly Limit</p>
-        </div>
-        <div className="App-data-table">
-          <VictoryStack colorScale={["red","blue","green","purple","black","grey","yellow","tomato", "orange", "gold"]}>
-            <VictoryArea
-              data={rollingSumAll}
-            />
-            <VictoryArea
-              data={rollingSumGroc}
-            />
-            <VictoryArea
-              data={rollingSumMerc}
-            />
-			<VictoryArea
-              data={rollingSumOther}
-            />
-			<VictoryArea
-              data={rollingSumEnt}
-            />
-			<VictoryArea
-              data={rollingSumDine}
-            />
-			<VictoryArea
-              data={rollingSumTrav}
-            />
-			<VictoryArea
-              data={rollingSumAuto}
-            />
-			<VictoryArea
-              data={rollingSumInsu}
-            />
-			<VictoryArea
-              data={rollingSumCloth}
-            />
+		<p>{total} {this.displays[8]}</p>
+		{checkBudget()}
+		<input type="checkbox" id="OtherBox"  onchange="this.invert()"/> Other 
+		
+		<div className="App-data-table">
+          <VictoryStack colorScale={["red","gold","green","purple","black","grey","yellow","tomato", "blue", "orange"]}>
+            {this.writeArea("Grocery",true)}
+			{this.writeArea("Merchandise",true)}
+			{this.writeArea("Entertainment",true)}
+			{this.writeArea("Dining",true)}
+			{this.writeArea("Travel",true)}
+			{this.writeArea("Gas/Automotive",true)}
+			{this.writeArea("Insurance",true)}
+			{this.writeArea("Clothing",true)}
+			{this.writeArea("Other",this.displays[8])}			
           </VictoryStack>
         </div>
-        <JsonToTable json = {hits} />
-      </header>
-    </div>
+		<MDBDataTable striped bordered hover data={data}/>
+	  </header>	  
+    
+	</div>
+	
     );
   
 }
